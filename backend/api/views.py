7c07a787844b0fa27c9b13dfb64573906f86c99e
RuslanRD <r.ruslan.d@yandex.kz>
2022-03-14 20:48:14 +0600

    update 14.03

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,6 @@
 from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
-from reportlab.pdfbase import pdfmetrics
-from reportlab.pdfbase.ttfonts import TTFont
-from reportlab.pdfgen import canvas
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
@@ -64,7 +61,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
     @action(detail=False, methods=['get'],
             permission_classes=[IsAuthenticated])
     def download_shopping_cart(self, request):
-        final_list = {}
+        shopping_list = {}
         ingredients = IngredientAmount.objects.values_list(
             'ingredient__name',
             'ingredient__measurement_unit'
@@ -73,36 +70,26 @@ class RecipeViewSet(viewsets.ModelViewSet):
         )
         for item in ingredients:
             name = item[0]
-            if name not in final_list:
-                final_list[name] = {
+            if name not in shopping_list:
+                shopping_list[name] = {
                     'measurement_unit': item[1],
                     'amount': item[2]
                 }
             else:
-                final_list[name]['amount'] += item[2]
+                shopping_list[name]['amount'] += item[2]
 
-        pdfmetrics.registerFont(
-            TTFont('teddy-bear', 'teddy-bear.ttf', 'UTF-8')
-        )
-        response = HttpResponse(content_type='application/pdf')
-        response['Content-Disposition'] = (
-            'attachment; '
-            'filename="shopping_list.pdf"'
-        )
-        page = canvas.Canvas(response)
-        page.setFont('teddy-bear', size=24)
-        page.drawString(200, 800, 'Список ингредиентов')
-        page.setFont('teddy-bear', size=16)
-        height = 750
-        for i, (name, data) in enumerate(final_list.items(), 1):
-            page.drawString(
-                75, height,
+        final_list = []
+        for i, (name, data) in enumerate(shopping_list.items(), 1):
+            final_list.append(
                 (f'<{i}> {name} - {data["amount"]}, '
                  f'{data["measurement_unit"]}')
             )
-            height -= 25
-        page.showPage()
-        page.save()
+        response = HttpResponse('\n'.join(final_list),
+                                content_type='text/plain; charset=UTF-8')
+        response['Content-Disposition'] = (
+            'attachment; '
+            'filename="shopping_list.txt"'
+        )
         return response
 
     def add_obj(self, model, user, pk):
